70981f0344998bfe94db85add429f7156e54963c
MartinMosqueira <tinchomosqueira@gmail.com>
2024-06-04 12:06:12 -0300

    feat(game): :construction: Added logic

--- Game.py
+++ Game.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from GameBoard import GameBoard
 from Store import Store
+from Client import Client
 import sys
 
 class Game:
@@ -21,6 +22,12 @@ class Game:
         # initialize store
         self.store = Store(5, 30)
 
+        # initialize boxes
+        self.store.start_boxes()
+
+        # initialize time boxes
+        self.store.start_time_boxes()
+
         self.clock = pygame.time.Clock()
         self.running = True
 
@@ -32,10 +39,48 @@ class Game:
 
             # draw board
             self.board.draw()
-            
-            # initialize boxes
-            self.store.start_boxes()
 
+            # client arrival simulation
+            if self.store.start_client():
+                print("Client arrived")
+                newClient = Client()
+
+                # assign client to a box or row
+                for i in range(0, self.store.numberBoxes):
+                    if len(self.store.boxes[i+1]) == 0:
+                        self.store.boxes[i+1].append(newClient)
+                        print("Client assigned to box", i+1)
+                        break
+                else:
+                    self.store.row.append(newClient)
+                    print("Client assigned to row")
+                    
+            
+            # check if a client arrives
+            if len(self.store.row) > 0:
+                for i in range(0, self.store.numberBoxes):
+                    if len(self.store.boxes[i+1]) == 0:
+                        self.store.boxes[i+1].append(self.store.row.pop(0))
+                        print("Client assigned to box", i+1)
+                        break
+            
+            # attention time simulation
+            for i in range(0, self.store.numberBoxes):
+                if len(self.store.boxes[i+1]) > 0:
+                    if self.store.timeBoxes[i+1] == None:
+                        self.store.timeBoxes[i+1] = self.store.attention_time_box()
+                        print("Attention time box", i+1, ":", self.store.timeBoxes[i+1])
+                    
+                    elif self.store.timeBoxes[i+1] < 0:
+                        self.store.boxes[i+1].pop(0)
+                        self.store.timeBoxes[i+1] = None
+                        print("Client finished in box", i+1)
+
+                    else:
+                        self.store.timeBoxes[i+1] -= 1
+                        print("Attention time box", i+1, ":", self.store.timeBoxes[i+1])
+                        
+                
             self.clock.tick(15)
             pygame.display.update()
             
--- Store.py
+++ Store.py
@@ -4,13 +4,23 @@ class Store:
     def __init__(self, numberBoxes, timeSeconds):
         self.numberBoxes = numberBoxes
         self.boxes={}
+        self.timeBoxes={}
         self.costBox=1000
         self.row = []
         self.timeSeconds = timeSeconds
+        self.clientProbability = 1/144
 
     def start_boxes(self):
-        for box in range(1,self.boxes+1):
+        for box in range(1,self.numberBoxes+1):
             self.boxes[box] = []
+
+    def start_time_boxes(self):
+        for box in range(1,self.numberBoxes+1):
+            self.timeBoxes[box] = None
 
     def attention_time_box(self):
         return np.random.normal(10, 5, 1)
+
+    def start_client(self):
+        if np.random.rand() < self.clientProbability:
+            return True
